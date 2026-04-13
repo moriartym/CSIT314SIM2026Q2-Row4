33e4685be0665f31c6ae691499c1871df426244a
GamonicXD <augustineserwx@gmail.com>
2026-04-13 15:58:50 +0800

    Update userService.js

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -12,7 +12,21 @@ class UserService {
     if (!user) throw new Error('User not found')
     return user
   }
-
+  async updateUserById(id, data) {
+    // Check if user exists
+    const user = await UserRepository.findById(id)
+    if (!user) throw new Error('User not found')
+  
+    // If password is being updated, hash it
+    if (data.password) {
+      data.password = await bcrypt.hash(data.password, 10)
+    }
+  
+    // Update user
+    const updatedUser = await UserRepository.updateById(id, data)
+  
+    return updatedUser
+  }
 
 }
 
